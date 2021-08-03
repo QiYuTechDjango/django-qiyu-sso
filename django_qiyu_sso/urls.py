3eb94eab046fe35fa8de06467a479453549e56d4
奇遇科技 <dev@qiyutech.tech>
2021-08-03 18:14:36 +0800

    支持 退出 登录

--- django_qiyu_sso/urls.py
+++ django_qiyu_sso/urls.py
@@ -6,11 +6,12 @@
 """
 from django.urls import path
 
-from .views import OAuth2LoginView, LoginSuccessView
+from .views import OAuth2LoginView, LoginSuccessView, LogoutView
 
 __all__ = ["sso_urls"]
 
 sso_urls = [
     path("accounts/login/", OAuth2LoginView.as_view(), name="login"),
+    path("accounts/logout/", LogoutView.as_view(), name="logout"),
     path("accounts/login/success/", LoginSuccessView.as_view(), name="login_success"),
 ]
